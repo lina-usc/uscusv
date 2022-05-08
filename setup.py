5abc9aac785f2e5c6e4cba402871e25bfcd19f0a
Christian O'Reilly <christian.oreilly@sc.edu>
2022-05-08 15:46:57 -0400

    setup debug

--- setup.py
+++ setup.py
@@ -22,5 +22,6 @@ if __name__ == "__main__":
         author="Christian O'Reilly",
         author_email='christian.oreilly@sc.edu',
         url='https://github.com/christian-oreilly/uscusv',
+        packages=['uscusv'],
         install_requires=install_requires
     )
