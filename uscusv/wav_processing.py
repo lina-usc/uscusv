acb3b3ed928c18be469ce818b2fb058586c27009
Sabah <sanis@email.sc.edu>
2023-02-22 17:33:55 -0500

    Fixed the threshold for tone estimation

--- uscusv/wav_processing.py
+++ uscusv/wav_processing.py
@@ -2,7 +2,7 @@ import mne
 import numpy as np
 from scipy.io import wavfile
 import pandas as pd
-
+from sklearn.cluster import KMeans
 
 def filter_small_changes(sig, N=20):
     sig[int( N /2):-int( N /2)] = np.stack([sig[i:-( N -i)] for i in np.arange(N)]).sum(axis=0) > N/ 2
@@ -25,7 +25,7 @@ def blocked_stft(data, wsize, nb_wsize_max=50000, verbose=True):
   return np.concatenate(tfs, axis=1)
 
 
-def get_tone_times(file_name, tone_freq=2000, wsize=2500, filter_N=20, threshold=50000, verbose=True):
+def get_tone_times(file_name, tone_freq=2000, wsize=2500, filter_N=20, threshold=None, verbose=True):
     sfreq, data = wavfile.read(file_name)
 
     dt = wsize / sfreq / 2
@@ -33,7 +33,13 @@ def get_tone_times(file_name, tone_freq=2000, wsize=2500, filter_N=20, threshold
     freqs = mne.time_frequency.stftfreq(wsize=wsize, sfreq=sfreq)
     #time = np.arange(0, dt * tf.shape[1], dt)
 
+    if threshold is None:
+      ecg = tf[freqs == tone_freq].squeeze()
+      mask = KMeans(n_clusters=2).fit_transform(ecg[:, None]).argmax(1) == 0
+      threshold = ecg[mask].min() if ecg[mask].mean() > ecg[~mask].mean() else ecg[~mask].min()
+
     thresholded_sig = tf[freqs == tone_freq].squeeze() > threshold
+
     filtered_sig = filter_small_changes(thresholded_sig, N=filter_N)
 
     up = np.where(np.diff(filtered_sig.astype(int)) == 1)[0]
